d4328735da4a24e2d8d61f1c0281e08e7fa815d4
RadoslavLG <radoslavlgeorgiev@gmail.com>
2017-09-21 18:02:21 +0300

    Createticket added with mostly working tests

---
+++ Traveller/Traveller.UnitTests/Commands/Creating/CreateTicketCommandTests/Constructor_Should.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Traveller.Commands.Creating;
+using Traveller.Core.Contracts;
+
+namespace Traveller.UnitTests.Commands.Creating.CreateTicketCommandTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ReturnAnInstanceOfTicketClass_WhenParametersAreCorrect()
+        {
+            // Arrange
+            var travellerFactoryMock = new Mock<ITravellerFactory>();
+            var databaseMock = new Mock<IDatabase>();
+
+            // Act
+            var ticketCreation = new CreateTicketCommand(travellerFactoryMock.Object, databaseMock.Object);
+
+            // Assert
+            Assert.IsNotNull(ticketCreation);
+        }
+
+        [TestMethod]
+        public void ThrowsException_WhenTravellerFactoryIsNull()
+        {
+            // Arrange
+            var databaseMock = new Mock<IDatabase>();
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new CreateTicketCommand(null, databaseMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowsException_WhenDatabaseIsNull()
+        {
+            // Arrange
+            var travellerFactoryMock = new Mock<ITravellerFactory>();
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new CreateTicketCommand(travellerFactoryMock.Object, null));
+        }
+    }
+}
--- Traveller/Traveller.UnitTests/Commands/Creating/CreateTicketCommandTests/Execute_Should.cs
+++ Traveller/Traveller.UnitTests/Commands/Creating/CreateTicketCommandTests/Execute_Should.cs
@@ -63,6 +63,50 @@ namespace Traveller.UnitTests.Commands.Creating.CreateTicketCommandTests
             var travellerFactoryMock = new Mock<ITravellerFactory>();
             var databaseMock = new Mock<IDatabase>();
             var firstJourneyMock = new Mock<IJourney>();
+            var firstTicketMock = new Mock<ITicket>();
+            var secondTicketMock = new Mock<ITicket>();
+            List<ITicket> tickets = new List<ITicket>()
+            {
+                firstTicketMock.Object
+            };
+            List<IJourney> journeys = new List<IJourney>()
+            {
+                firstJourneyMock.Object
+            };
+
+            decimal administrativeCosts = 100;
+
+            List<string> parameters = new List<string>()
+            {
+                "0",
+                "100"
+            };
+
+            string expectedResult = "Ticket with ID 1 was created.";
+
+            travellerFactoryMock.Setup(m => m.CreateTicket(firstJourneyMock.Object, administrativeCosts))
+                .Returns(secondTicketMock.Object);
+
+            databaseMock.Setup(m => m.Journeys).Returns(journeys);
+            databaseMock.Setup(m => m.Tickets).Returns(tickets);
+
+            CreateTicketCommand command =
+                new CreateTicketCommand(travellerFactoryMock.Object, databaseMock.Object);
+
+            // Act
+            string result = command.Execute(parameters);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void ThrowsExceptionWithSpecificText_WhenParametersAreNotInCorrectFormat()
+        {
+            // Arrange
+            var travellerFactoryMock = new Mock<ITravellerFactory>();
+            var databaseMock = new Mock<IDatabase>();
+            var firstJourneyMock = new Mock<IJourney>();
             var secondJourneyMock = new Mock<IJourney>();
             var ticketMock = new Mock<ITicket>();
             List<ITicket> tickets = new List<ITicket>();
@@ -80,21 +124,21 @@ namespace Traveller.UnitTests.Commands.Creating.CreateTicketCommandTests
                 "100"
             };
 
-            travellerFactoryMock.Setup(m => m.CreateTicket(secondJourneyMock.Object, administrativeCosts))
-                .Returns(ticketMock.Object);
+            string expectedResult = "Failed to parse CreateTicket command parameters.";
 
-            databaseMock.Setup(m => m.Journeys).Returns(journeys);
+            travellerFactoryMock.Setup(m => m.CreateTicket(secondJourneyMock.Object, administrativeCosts)).Returns(ticketMock.Object);
+
+            databaseMock.Setup(m => m.Journeys).Throws(new ArgumentException());
             databaseMock.Setup(m => m.Tickets).Returns(tickets);
 
             CreateTicketCommand command =
                 new CreateTicketCommand(travellerFactoryMock.Object, databaseMock.Object);
 
             // Act
-            command.Execute(parameters);
+            string result = command.Execute(parameters);
 
             // Assert
-            Assert.AreEqual(1, databaseMock.Object.Tickets.Count);
-            Assert.AreSame(ticketMock.Object, databaseMock.Object.Tickets.Single());
+            Assert.AreEqual(expectedResult, result);
         }
     }
 }
